d8cee9a19a4586bc24ec0f27368cf6305833637a
alexcrist <alexecrist@gmail.com>
2025-02-24 14:44:32 -0800

    Add react-redux-vite skeleton

--- main.js
+++
@@ -1,7 +0,0 @@
-const count = document.querySelector("#count");
-const button = document.querySelector("#button-1");
-button.addEventListener("click", () => {
-    const previousValue = Number(count.innerText);
-    const nextValue = previousValue + 1;
-    count.innerText = nextValue;
-});
