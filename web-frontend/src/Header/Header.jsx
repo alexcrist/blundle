589cb58ffd55e2a24bbfabfbaf88be68a74a25b3
alexcrist <alexecrist@gmail.com>
2025-02-28 14:47:55 -0800

    Tweak header

--- web-frontend/src/Header/Header.jsx
+++ web-frontend/src/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
         <div className={styles.container}>
             <div className={styles.title}>
                 <div className={styles.titleStart}>Blundle</div>
-                <div className={styles.titleEnd}>: Countries</div>
+                <div className={styles.titleEnd}>- Countries</div>
             </div>
         </div>
     );
